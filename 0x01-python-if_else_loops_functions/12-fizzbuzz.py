248c2642836abe00e7ff7c3640b9ae1bad66da58
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-08 05:25:56 -0700

    ok

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    str1 = ""
+    for number in range(1, 100):
+        if (number % 3) == 0 and (number % 5) == 0:
+            str1 += "FizzBuzz "
+        elif (number % 3) == 0:
+            str1 += "Fizz "
+        elif (number % 5) == 0:
+            str1 += "Buzz"
+        else:
+            str1 += "{} ".format(number)
+    print(str1)
+fizzbuzz()
