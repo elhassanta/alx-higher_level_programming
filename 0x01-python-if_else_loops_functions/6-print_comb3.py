c06f4d808213aae43b31f400e7b221b51599a1d3
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-08 04:14:46 -0700

    ok

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for number in range(0, 10):
+    for i in range(number + 1, 10):
+        if i == 9 and number == 8:
+            print("{}{}".format(number, i))
+        else:
+            print("{}{},".format(number, i), end = " ")
