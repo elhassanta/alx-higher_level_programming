f8bdcf22c5281dab364a49e365e55cdddae0fec5
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-10 05:34:13 -0700

    0x02pthn

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-
-    dir(hidden_4.pyc)
+    hidd = dir(hidden_4)
+    for hid in hidd:
+        if hid[0] != '_':
+            print(hid)
