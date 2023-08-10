69060cb1e2502756b3bc4790a9f7acba6ba470c2
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-10 04:17:55 -0700

    0x02pthn

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(len(argv)))
+        print("{} arguments:".format(len(argv) - 1))
         for arg in range(1, len(argv)):
             print("{}: {}".format(arg, argv[arg]))
-
