20609a679a686c767991486d27f60d256f0cc0fd
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-11 06:42:50 -0700

    ok

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """This function prints a matrix of integer
+
+    Arg:
+
+    matrix: parameter list of list of integers
+    """
+    for elements in matrix:
+        for element in range(0,len(elements)):
+            if element != len(elements) - 1:
+                print("{}".format(elements[element]), end=" ")
+            else:
+                print("{}".format(elements[element]))
