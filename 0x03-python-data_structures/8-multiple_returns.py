060926397e0c4360a5072dfd71293fef46ca1f8b
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-11 07:41:07 -0700

    ok

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """My function returns a tuple with the length of a string and its first character.
+
+    Arg:
+    
+    sentence: parameter string
+
+    Return:
+    
+    return tuple of two elements
+    """
+    if len(sentence) == 0:
+        return None
+    return tuple(len(sentence), sentence[0])
