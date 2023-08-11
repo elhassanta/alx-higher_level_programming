f5fee187874c62f00676fe266a5efd45c56a130c
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-11 07:27:21 -0700

    ok

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """My function add two tuples
+
+    Args:
+
+    tuple_a: first parameter tuple of integers
+    tuple_b: second parameter tuple of integers
+
+    Return:
+
+    """
+    if len(tuple_a) == 2 and len(tuple_b) == 2:
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    elif len(tuple_a) == 2 and len(tuple_b) == 1:
+        return (tuple_a[0] + tuple_b[0], tuple_a[1])
+    elif len(tuple_a) == 2 and len(tuple_b) == 0:
+        return (tuple_a[0], tuple_a[1])
+    elif len(tuple_a) == 1 and len(tuple_b) == 2:
+        return (tuple_b[0] + tuple_a[0], tuple_b[1])
+    elif len(tuple_a) == 0 and len(tuple_b) == 2:
+        return (tuple_b[0], tuple_b[1])
+    else:
+        return tuple()
