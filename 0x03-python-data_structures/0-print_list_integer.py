e40fc8de47b411566d15f381c92f3bda14ebfe39
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-12 02:49:27 -0700

    ok

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """My print integer function
+if __name__ == "__main__"
+    def print_list_integer(my_list=[]):
+        """My print integer function
 
-    Args:
-        my_list: list of integer
-    """
-    if my_list != []:
-        for number in my_list:
+        Args:
+            my_list: list of integer
+        """
+         for number in my_list:
             print("{}".format(number))
