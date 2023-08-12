7714c51d5abb51be327d207c81522adbd0286c40
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-12 02:43:37 -0700

    ok

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,8 +3,7 @@ def print_list_integer(my_list=[]):
     """My print integer function
 
     Args:
-
-    my_list: list of integer
+        my_list: list of integer
     """
     for number in my_list:
         print("{}".format(number))
