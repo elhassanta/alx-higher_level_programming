f7bc7e08391b6f716d50bef0ebde734f4d804160
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-12 08:52:46 -0700

    task-c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,2 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <Python.h>
+/**
+* print_python_list_info - Prints basic info about Python lists.
+* @p: A PyObject list.
+*/
+void print_python_list_info(PyObject *p)
+{
+	int alloc, size;
+	PyObject *obj;
+	siz = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+}
