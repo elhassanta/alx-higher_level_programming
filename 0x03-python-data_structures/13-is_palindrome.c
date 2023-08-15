3af047a5ebb26d1f4818436668681000618f1d4d
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-15 05:20:31 -0700

    ok

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,19 +5,20 @@
  *@fnode: parameter to first node
  *Return: return reversed linked list
  */
+#include "lists.h"
+
 void rev_list(listint_t **head, listint_t *fnode)
 {
-	listint_t *node = NULL;
-
-	if (fnode->next == NULL)
-	{
-		*head = fnode;
-		return;
-	}
-	rev_list(head, fnode->next);
-	node = fnode->next;
-	node->next = fnode;
-	fnode->next = NULL;
+listint_t *node = NULL;
+if (fnode->next == NULL)
+{
+*head = fnode;
+return;
+}
+rev_list(head, fnode->next);
+node = fnode->next;
+node->next = fnode;
+fnode->next = NULL;
 }
 /**
  *is_palindrome - check if a linked list is palindrome
@@ -26,39 +27,41 @@ void rev_list(listint_t **head, listint_t *fnode)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = (*head), *comp1, *comp2;
-	listint_t **head2 = NULL;
-	int len = 0, count = 0;
+listint_t *temp = (*head), *comp1, *comp2;
+listint_t *head2 = NULL;
+int len = 0, count = 0;
 
-	if ((!(*head)) || !head || (*head)->next == NULL)
-		return (1);
-	while (temp->next)
-	{
-		temp = temp->next;
-		len++;
-	}
-	temp = *head;
-	while (count <= len / 2)
-	{
-		temp = temp->next;
-		count++;
-	}
-	head2 = &temp;
-	rev_list(head2, temp);
-	comp1 = *head;
-	comp2 = *head2;
-	count = 0;
-	while (count <= len / 2)
-	{
-		if (comp1->n != comp2->n)
-		{
-			rev_list(head2, *head2);
-			return (0);
-		}
-		comp1 = comp1->next;
-		comp2 = comp2->next;
-		count++;
-	}
-	rev_list(head2, *head2);
-	return (1);
+if ((!(*head)) || !head || (*head)->next == NULL)
+return (0);
+while (temp)
+{
+temp = temp->next;
+len++;
+}
+temp = *head;
+while (count < len / 2 - 1)
+{
+temp = temp->next;
+count++;
+}
+if ((len % 2) == 0 && temp->n != temp->next->n)
+return (0);
+temp = temp->next->next;
+head2 = temp;
+rev_list(&head2, temp);
+comp1 = *head;
+count = 0;
+while (count < len / 2 - 1)
+{
+if (comp1->n != temp->n)
+{
+rev_list(&head2, head2);
+return (0);
+}
+comp1 = comp1->next;
+temp = temp->next;
+count++;
+}
+rev_list(&head2, head2);
+return (1);
 }
