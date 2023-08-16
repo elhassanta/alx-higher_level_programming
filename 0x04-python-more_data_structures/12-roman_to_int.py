07ae1b582ecacfaa971e215dc695fce72a17a632
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-16 09:11:58 -0700

    ok

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    number = 0;
+    number = 0
     for letter in roman_string:
         number = values[letter] + number
     return number
