0dd62e0ee82e82ff7baf3156b9c22f79e1809186
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-16 07:02:14 -0700

    ok

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    first = 0
+    max_score_key = None
+    for k, v in a_dictionary.items():
+        if first == 0:
+            first = 1
+            max_score = v
+            max_score_key = k
+        if v > max_score:
+            v = max_score
+            max_score_key = k
+    return max_score_key
