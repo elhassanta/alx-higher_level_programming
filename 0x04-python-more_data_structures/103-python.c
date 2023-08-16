32b764ec558e5c44965c38a8f17cdd238f392cb8
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-16 14:04:05 -0700

    ok

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,22 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+/**
+ *print_python_list - this function print some basic info about Python lists
+ *@p: parameter pointer
+ */
+void print_python_list(PyObject *p)
+{
+
+}
+
+/**
+ *print_python_bytes - this function print some basic info about Python lists
+ *@p: parameter pointer
+ */
+void print_python_bytes(PyObject *p)
+{
+
+}
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    number = 0
-    for letter in roman_string:
-        number = values[letter] + number
+    list1 = list()
+    for str1 in roman_string:
+        list1.append(str1)
+    list1.reverse()
+    number = values[list1[0]]
+    check = list1[0]
+    for index in range(1, len(list1)):
+        if values[list1[index]] >= values[check]:
+            check = list1[index]
+            number = number + values[list1[index]]
+        else:
+            number = number - values[list1[index]]
     return number
