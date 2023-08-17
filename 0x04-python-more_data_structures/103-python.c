36d2fc3ec557acbae689df31412a40384b35ea78
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-17 16:29:23 -0700

    ok

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -5,8 +5,7 @@ void print_python_bytes(PyObject *p);
 
 /**
 * print_python_bytes - Prints bytes information
-**
-@p: Python Object
+*@p: Python Object
 * Return: no return
 */
 void print_python_bytes(PyObject *p)
@@ -34,4 +33,26 @@ printf(" %02x", str[i]);
 else
 printf(" %02x", 256 + str[i]);
 printf("\n");
+}
+/**
+* print_python_list - Prints list information
+*@p: Python Object
+* Return: no return
+*/
+void print_python_list(PyObject *p)
+{
+long int size, i;
+PyListObject *list;
+PyObject *obj;
+size = ((PyVarObject *)(p))->ob_size;
+list = (PyListObject *)p;
+printf("[*] Python list info\n");
+printf("[*] Size of the Python List = %ld\n", size);
+printf("[*] Allocated = %ld\n", list->allocated);
+for (i = 0; i < size; i++)
+{
+obj = ((PyListObject *)p)->ob_item[i];
+printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+if (PyBytes_Check(obj))
+print_python_bytes(obj);
 }
