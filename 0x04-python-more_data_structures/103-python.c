23cbf2342355f9b79eb0b00648f1c6871026d2c3
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-17 16:25:34 -0700

    ok

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,22 +1,37 @@
 #include <Python.h>
-
+#include <stdio.h>
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 
 /**
- *print_python_list - this function print some basic info about Python lists
- *@p: parameter pointer
- */
-void print_python_list(PyObject *p)
-{
-
-}
-
-/**
- *print_python_bytes - this function print some basic info about Python lists
- *@p: parameter pointer
- */
+* print_python_bytes - Prints bytes information
+**
+@p: Python Object
+* Return: no return
+*/
 void print_python_bytes(PyObject *p)
 {
-
+char *str;
+long int size, i, limit;
+printf("[.] bytes object info\n");
+if (!PyBytes_Check(p))
+{
+printf(" [ERROR] Invalid Bytes Object\n");
+return;
+}
+size = ((PyVarObject *)(p))->ob_size;
+str = ((PyBytesObject *)p)->ob_sval;
+printf(" size: %ld\n", size);
+printf(" trying string: %s\n", str);
+if (size >= 10)
+limit = 10;
+else
+limit = size + 1;
+printf(" first %ld bytes:", limit);
+for (i = 0; i < limit; i++)
+if (str[i] >= 0)
+printf(" %02x", str[i]);
+else
+printf(" %02x", 256 + str[i]);
+printf("\n");
 }
