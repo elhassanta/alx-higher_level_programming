28a00f53a6b7677827083356806a92d5419aedca
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-28 08:09:44 -0700

    ok

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -18,23 +18,3 @@ def safe_function(fct, *args):
     except ZeroDivisionError  as err:
         tot_error = "Exception: " + str(err) + "\n"
         write(tot_error)
-def my_div(a, b):
-    return a / b
-
-result = safe_function(my_div, 10, 2)
-print("result of my_div: {}".format(result))
-
-result = safe_function(my_div, 10, 0)
-print("result of my_div: {}".format(result))
-
-
-def print_list(my_list, len):
-    i = 0
-    while i < len:
-        print(my_list[i])
-        i += 1
-    return len
-
-result = safe_function(print_list, [1, 2, 3, 4], 10)
-print("result of print_list: {}".format(result))
-
