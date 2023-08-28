d3409ce6557da85fdb86d04465ea46f21fe6101d
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-28 03:24:26 -0700

    ok

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return value
+        return 1
     except ValueError:
         return 0
---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    element_added = 0
+    elements = []
+    print(list_length)
+    for i in range(0, list_length):
+        print(i)
+        try:
+            print(my_list_1[i], my_list_2[i])
+            elements[element_added] = my_list_1[i] / my_list_2[i]
+            element_added = element_added + 1
+        except IndexError:
+            print("out of range")
+        finally:
+            print("finally")
+    return elements
+
+
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+print(max(len([1, 2, 3]), len([2, 3, 4])))
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
