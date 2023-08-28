416e76fb57be67797a60680548e5feb8a834dbea
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-28 07:37:02 -0700

    ok

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as err:
+        tot_error = "Exception: " + str(err) + "\n"
+        sys.stderr.write(tot_error)
+        return False
+    except TypeError as err:
+        tot_error = "Exception: " + err + "\n"
+        sys.stderr.write(tot_error)
+        return False
