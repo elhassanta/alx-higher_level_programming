d3604ae69b5c33553271c677ce874c8ca23aab3c
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-28 08:12:11 -0700

    ok

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
-
-
 write = sys.stderr.write
+
+
 def safe_function(fct, *args):
-    (arg1,arg2) = args
+    (arg1, arg2) = args
     print("ok *", arg1, arg2)
     try:
         result = fct(arg1, arg2)
@@ -15,6 +15,6 @@ def safe_function(fct, *args):
     except TypeError as err:
         tot_error = "Exception: " + str(err) + "\n"
         write(tot_error)
-    except ZeroDivisionError  as err:
+    except ZeroDivisionError as err:
         tot_error = "Exception: " + str(err) + "\n"
         write(tot_error)
