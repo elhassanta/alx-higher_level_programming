86b4f88d251cee205348393f40d222b68a2d518e
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-28 10:03:58 -0700

    ok

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+#include <stdio.h>
+/**
+ *print_python_list - this function will print info about a list
+ *@p: parameter pointer of type object
+ */
+void print_python_list(PyObject *p)
+{
+	return;
+}
+/**
+ *print_python_bytes - this function will print info about a list
+ *@p: parameter pointer of PyObject
+ */
+void print_python_bytes(PyObject *p)
+{
+	return;
+}
+/**
+ *print_python_float - this function will print info about a list
+ *@p: parameter pointer of PyObject
+ */
+void print_python_float(PyObject *p)
+{
+	return;
+}
