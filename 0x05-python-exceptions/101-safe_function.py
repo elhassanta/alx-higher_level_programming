3b960744c7287a9b5869f185a61195b71e3f8626
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-28 08:15:08 -0700

    ok

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,16 +5,18 @@ write = sys.stderr.write
 
 def safe_function(fct, *args):
     (arg1, arg2) = args
-    print("ok *", arg1, arg2)
     try:
         result = fct(arg1, arg2)
         return result
     except IndexError as err:
         tot_error = "Exception: " + str(err) + "\n"
         write(tot_error)
+        return None
     except TypeError as err:
         tot_error = "Exception: " + str(err) + "\n"
         write(tot_error)
+        return None
     except ZeroDivisionError as err:
         tot_error = "Exception: " + str(err) + "\n"
         write(tot_error)
+        return None
