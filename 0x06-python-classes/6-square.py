c7fc823b99e12ef20ef7a1d3213db6539320f18a
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-29 11:09:37 -0700

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,18 +42,3 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.size = value
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
