29b24aa19514f9b320f6c5d061d87a93f9a45abe
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-30 11:48:27 -0700

    tsk-102

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
+"""magic class"""
+
 import math
 
 
 class MagicClass:
+    """ begining of magic class"""
 
     def __init__(self, radius=0):
+        """constructor of disk class
+
+        args:
+            radius: param int or float
+        """
 
         if type(radius) is not float and type(radius) is not int:
             raise TypeError("radius must be a number")
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
-        return ((math.pi) * self._MagicClass__radius ** 2)
+        """this method calculate the erea"""
+        return ((math.pi) * self.__radius ** 2)
 
     def circumference(self):
-        return (2 * (math.pi) * self._MagicClass__radius)
+        """this method calculate the perimeter of disk"""
+        return (2 * (math.pi) * __radius)
