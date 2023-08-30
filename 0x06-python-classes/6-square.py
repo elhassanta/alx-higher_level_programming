ec4ee0095159c6d032ab5886b0a036f3f6add1c5
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-30 00:58:42 -0700

    corr-4

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,8 +23,9 @@ class Square:
         if isinstance(position, tuple) and len(position) == 2:
             if isinstance(position[0], int) and isinstance(position[1], int):
                 self.__position = tuple(position)
-            else if position[0] < 0 or position[1] < 0:
-                raise ValueError("position must be a tuple of 2 positive integers")
+            if position[0] < 0 or position[1] < 0:
+                raise ValueError("position must be a tuple of \
+                        2 positive integers")
             else:
                 raise TypeError(errstr)
         else:
