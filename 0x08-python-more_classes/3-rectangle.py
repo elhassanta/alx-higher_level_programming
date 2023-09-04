7e227c75a894e85b0d60602a774fa24f9d67811a
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-04 09:06:25 -0700

    tasks

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""this is the discription model"""
+
+
+class Rectangle:
+    """this is a tamplate to create rectangle object"""
+
+    def __init__(self,  width=0, height=0):
+        """this is the constroctor method"""
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """this the getter method of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """this is the setter method of height"""
+        if not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        """this the getter method of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """this is the setter method of height"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    def perimeter(self):
+        """this method calculate perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def area(self):
+        """this method calculate the area of rectangle"""
+        return (self.__width * self.__height)
+    def __str__(self):
+        str = ""
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                str = str + "#"
+            if i < self.__height - 1:
+                str = str + "\n"
+        return str
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print(str(my_rectangle))
+print(repr(my_rectangle))
