8ac6a07160c50c9234580a9d87ea14ef108de1c7
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-04 09:52:02 -0700

    tasks

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""this is the discription model"""
+
+
+class Rectangle:
+    """this is a tamplate to create rectangle object"""
+
+    __number_of_instances = 0
+    def __init__(self,  width=0, height=0):
+        """this is the constroctor method"""
+        self.width = width
+        self.height = height
+        Rectangle.__number_of_instances = Rectangle__number_of_instances + 1
+
+    def number_of_instances(self):
+        return (self.__number_of_instance)
+
+    @property
+    def height(self):
+        """this the getter method of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """this is the setter method of height"""
+        if not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        if height < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = height
+
+    @property
+    def width(self):
+        """this the getter method of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """this is the setter method of height"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    def perimeter(self):
+        """this method calculate perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width + self.__height) * 2
+
+    def area(self):
+        """this method calculate the area of rectangle"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """this is the string method"""
+        str = ""
+        if self.__width == 0 or self.__height == 0:
+            return str
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                str = str + "#"
+            if i < self.__height - 1:
+                str = str + "\n"
+        return str
+
+    def __repr__(self):
+        """ this method allow us to create a copy of an instance"""
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """this is going pop out in deletion"""
+        Rectangle.__number_of_instances = Rectangle__number_of_instances - 1
+        print("Bye rectangle...")
