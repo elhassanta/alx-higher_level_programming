b39adc69c9f16df3cccef08f355ef925e7584a0a
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-05 00:08:37 -0700

    task-0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this is my model"""
+
+def add_integer(a, b=98):
+    """this is my function"""
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,9 @@
+0x07. Python - Test-driven development
+Python
+UnitTests
+TDD
+ By: Guillaume
+ Weight: 1
+ Project will start Aug 31, 2023 4:00 AM, must end by Sep 6, 2023 4:00 AM
+ Checker will be released at Sep 6, 2023 4:00 AM
+ An auto review will be launched at the deadline
