49ce9ae57dc3e74c10b276471f2580d4236529dd
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-11 10:26:54 -0700

    tasks-inheretence

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""here is my model"""
+
+
+def lookup(obj):
+    """this is my function for looking
+    Args:
+        take obj as argument
+    Return:
+        return an array of what attribute the obj hold
+        """
+    return (dir(obj))
