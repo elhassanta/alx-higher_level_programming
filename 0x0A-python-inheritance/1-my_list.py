818d75449853c28a33d384421715d5ed42daf7fc
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-11 13:17:18 -0700

    added-1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""this is the place of our models"""
+
+
 class MyList(list):
     """this class will inherit lis class"""
 
     def print_sorted(self):
+        """this methode print a sorted array"""
         print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("./tests/1-my_list.txt")
---
+++ 0x0A-python-inheritance/test.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+print(dir(__import__("1-my_list")))
