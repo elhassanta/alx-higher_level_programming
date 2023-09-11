fc6a51eba123806510694476228ad02a86acc2d7
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-11 13:24:24 -0700

    added-1

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this the place for my models"""
+
+
+def is_same_class(obj, a_class):
+    """this function will return true if obj is instance of a given class"""
+    if isinstance(obj, a_class):
+        return True
+    return False
