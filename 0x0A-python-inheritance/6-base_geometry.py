9cb9434885a7d6c49846cf4ba956018a27c75bc8
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-11 13:53:50 -0700

    added-1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""model's place"""
+
+
+class BaseGeometry:
+    """this is an empty class"""
+    def area(self):
+        """this method will calculate the area"""
+        raise Exception("area() is not implemented")
