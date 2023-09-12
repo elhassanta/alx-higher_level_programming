cb46da1f182f8c8bfbd71642196d873b71c8861e
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 14:55:31 -0700

    inout-tasks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """function that writes a string to a text file
     (UTF8) and returns the number of characters written"""
     with open(filename, mode="w", encoding="utf-8") as file1:
-        file1.write(text)
+        return file1.write(text)
