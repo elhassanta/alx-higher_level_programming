f855cfaf93bc0facecd1dab1ba5e9583300e85dc
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 09:11:34 -0700

    input-output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,8 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """function that writes a string to a text file 
+    """function that writes a string to a text file
     (UTF8) and returns the number of characters written"""
     with open(filename, mode="w", encoding="utf-8") as file1:
         file1.write(text)
-
