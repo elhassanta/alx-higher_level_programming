143104505532fb2268d93b26dcdd7b6b0e3f1168
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 09:16:09 -0700

    input-output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """this is a comment for my model"""
 
+
 def read_file(filename=""):
     """this my function which open a file"""
     if filename == "":
-        return
-    with open(filename, mode="r", encoding="utf-8") as myFile:
-        str1 = myFile.read()
+        with open(filename, mode="r", encoding="utf-8") as myFile:
+            str1 = myFile.read()
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,6 @@
 def write_file(filename="", text=""):
     """function that writes a string to a text file
     (UTF8) and returns the number of characters written"""
-    with open(filename, mode="w", encoding="utf-8") as file1:
-        file1.write(text)
+    if filename != "" or not isinstance(filename, str):
+        with open(filename, mode="w", encoding="utf-8") as file1:
+            file1.write(text)
