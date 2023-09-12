d8875e2e384b8d184ba5e3d6e43d12bbc9fc0a12
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 10:22:20 -0700

    in-out

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -5,5 +5,4 @@ import json
 
 def to_json_string(my_obj):
     """function that returns the JSON representation of an object (string)"""
-    serialized_data = json.dumps(my_obj)
-    return json.loads(serialized_data)
+    return json.dumps(my_obj)
