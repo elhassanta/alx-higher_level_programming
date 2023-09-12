694c2691a1753abe19de17d9c0bf1107c9412d54
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-11 17:53:51 -0700

    tasks

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""place of my comment model"""
+
+
+def add_attribute(obj, attribute, value):
+    """this function add attribute if it is possible"""
+    setattr(obj, attribute, value)
