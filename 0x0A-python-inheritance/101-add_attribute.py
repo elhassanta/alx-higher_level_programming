751a9f0ea9b01930941dd3e3d55ac68ad1a576e7
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-11 17:57:05 -0700

    tasks

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,4 +4,6 @@
 
 def add_attribute(obj, attribute, value):
     """this function add attribute if it is possible"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
     setattr(obj, attribute, value)
