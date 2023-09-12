58a04d5b40b30fa9bdfc6114631c70b18340f807
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-11 17:44:28 -0700

    tasks

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -10,7 +10,7 @@ class MyInt(int):
     """
     def __eq__(self, value):
         """overide int __eq__"""
-        return self.real =! value
+        return self.real != value
 
     def __ne__(self, value):
         """overide __ne__ method"""
