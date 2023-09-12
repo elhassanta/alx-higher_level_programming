60fd28684efd223917c0a0bc55093c36baa3107e
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 14:48:19 -0700

    inout-tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     if filename == "":
         with open(filename, mode="r", encoding="utf-8") as myFile:
             str1 = myFile.read()
-            myFile.close()
+            print(str1)
