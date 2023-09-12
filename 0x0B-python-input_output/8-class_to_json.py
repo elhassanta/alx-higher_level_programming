9df04fec3292bee93db735634d98ba9a1ef47b86
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 16:12:49 -0700

    in-out

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """this is a comment for my models"""
+import json
 
 
 def class_to_json(obj):
     """unction that returns the dictionary description with simple
     data structure (list, dictionary,string, integer and boolean)
     for JSON serialization of an object"""
-    dict_attributes = vars(a)
+    dict_attributes = vars(obj)
     return json.dumps(dict_attributes)
