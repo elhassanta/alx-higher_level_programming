ab5052697c01ada1db330a17a298c4b323a5d948
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 09:09:41 -0700

    input-output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """this is my model"""
-import os
 
 
 def write_file(filename="", text=""):
     """function that writes a string to a text file 
     (UTF8) and returns the number of characters written"""
+    with open(filename, mode="w", encoding="utf-8") as file1:
+        file1.write(text)
 
