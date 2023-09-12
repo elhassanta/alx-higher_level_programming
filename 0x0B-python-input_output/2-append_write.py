945a1993a7bd08bf7f6df50231d4bf3377a3795a
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 09:28:36 -0700

    input-output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is first comment"""
+
+
+def append_write(filename="", text=""):
+    """this my appending function into a text file"""
+    if type(filename).__name__ == str.__name__:
+        with open(filename, mode="a", encoding="utf-8") as file1:
+            file1.write(text)
