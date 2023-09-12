e62b46296e537426212931855cd5b105d28c6dd9
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 10:16:49 -0700

    in-out

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """this is my code"""
-import pickle
+import json
 
 
 def to_json_string(my_obj):
     """function that returns the JSON representation of an object (string)"""
-    if type(my_obj).__name__ == set.__name__:
-        raise TypeError("{} is not JSON serializable".format(my_obj))
-    serialized_data = pickle.dumps(my_obj)
-    return pickle.loads(serialized_data)
+    serialized_data = json.dumps(my_obj)
+    return json.loads(serialized_data)
