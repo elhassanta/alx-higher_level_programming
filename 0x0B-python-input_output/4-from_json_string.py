b5186c881bb9d6918b69656c592417ddca82fe01
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 10:33:56 -0700

    in-out

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this is my code"""
+import json
+
+
+def from_json_string(my_str):
+    """function that returns the JSON representation of an object (string)"""
+    return json.loads(json_string)
