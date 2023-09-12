691f9802fb56299094d79385cb0807d554ed2fcb
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 15:14:23 -0700

    inout-tasks

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""this place ofcomment that describes my models"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file”"""
+    with open(filename, mode="r", encoding="utf-8") as file1:
+        str1 = file1.read()
+        return json.loads(str1) 
