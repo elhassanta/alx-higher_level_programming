7e34f7bd08da64b035e22c2feab55fb1624596c9
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 14:31:17 -0700

    inout-tasks

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,4 +5,4 @@ import json
 
 def from_json_string(my_str):
     """function that returns the JSON representation of an object (string)"""
-    return json.loads(json_string)
+    return json.loads(my_str)
