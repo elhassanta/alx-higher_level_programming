eebc47acc1f31a2ca9db723f6586c56c7873bdaf
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 14:58:06 -0700

    inout-tasks

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ def append_write(filename="", text=""):
     """this my appending function into a text file"""
     if type(filename).__name__ == str.__name__:
         with open(filename, mode="a", encoding="utf-8") as file1:
-            return file1.write(text
+            return file1.write(text)
