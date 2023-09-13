8d377057c538f1f57f546007c87d7d1f8a6f3c3d
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 19:14:56 -0700

    task-10

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,12 +2,7 @@
 """this is my class models"""
 
 
-class Student:
-    """this is my class struct"""
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self):
-        return json.dumps(vars(self))
+def pascal_triangle(n):
+    """Technical interview preparation"""
+    if n <= 0:
+        return []
