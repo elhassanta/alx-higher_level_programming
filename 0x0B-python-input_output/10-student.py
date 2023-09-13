f3b59749175a15eec5cd195d595c2650a417de0b
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 18:29:06 -0700

    task-10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,15 +9,26 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """unction that returns the dictionary description with simple
         data structure (list, dictionary,string, integer and boolean)
         for JSON serialization of an object"""
         dict_attributes = self.__dict__
         dic = {}
+        dic1 = {}
         for k, v in dict_attributes.items():
-            if isinstance(v, list):
-                dic[k] = v[0]
-            else if isinstance(v, (int, dict, bool, str)):
-                dic[k] = v
+            if isinstance(v, (int, list, dict, bool, str)):
+                if attrs != None:
+                    if k in attrs:
+                        dic[k] = v
+                else:
+                    dic[k] = v
         return dic
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
