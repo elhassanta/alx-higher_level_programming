28f2f07887ed5773d66088d77ce38d02ba546eb9
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 20:00:20 -0700

    task-10

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,3 +6,12 @@ def pascal_triangle(n):
     """Technical interview preparation"""
     if n <= 0:
         return []
+    triangle = [[1]]
+    while len(triangle) != n:
+        tri = triangle[-1]
+        line = [1]
+        for i in range(len(tri) - 1):
+            line.append(tri[i] + tri[i + 1])
+        line.append(1)
+        triangle.append(line)
+    return triangle
