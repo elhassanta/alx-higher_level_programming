0c347f65c09abeb6c18ec2a5972657a0717173f8
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-13 00:42:07 -0700

    corr-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,10 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-
-arguments = sys.argv[1:]
+try:
+    a = load_from_json_file("add_item.json")
+except Exception as err:
+    a = []
+arguments = sys.argv[1:] + a
 save_to_json_file(arguments, "add_item.json")
 load_from_json_file("add_item.json")
