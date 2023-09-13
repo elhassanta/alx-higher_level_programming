7816f222bb572658c8e13f332bb8c3378d7ba0c6
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-13 16:34:25 -0700

    corr

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,18 +1,51 @@
 #!/usr/bin/python3
-"""this is comment for my models"""
-import sys
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C), 
+"""
 
 
-read = sys.stdin.readline
-i = 1
-a = []
-while i < 3:
-    b = read()
-    a.append(b)
-    i = i + 1
-total_size = 0
-for j in a:
-    elem = j.split()[8]
-    print(elem)
-    total_size += int(elem)
-print(total_size)
+def print_status(size, status):
+    """Print status"""
+    print("File size: {}".format(size))
+    arr_keys = sorted(status)
+    for key in arr_keys:
+        print("{}: {}".format(key, status[key]))
+
+
+if __name__ == "__main__":
+    import sys
+
+    count = 0
+    size = 0
+    status_codes = dict()
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_status(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            words = line.split()
+
+            try:
+                size += int(words[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if words[-2] in valid_codes:
+                    if status_codes.get(words[-2], -1) == -1:
+                        status_codes[words[-2]] = 1
+                    else:
+                        status_codes[words[-2]] += 1
+            except IndexError:
+                pass
+
+        print_status(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+    raise
