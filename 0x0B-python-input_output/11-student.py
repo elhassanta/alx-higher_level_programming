f2f4722aab85b341582e35caf39a7da61f102444
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-12 18:43:19 -0700

    task-10

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -9,5 +9,24 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        return json.dumps(vars(self))
+    def to_json(self, attrs=None):
+        """unction that returns the dictionary description with simple
+        data structure (list, dictionary,string, integer and boolean)
+        for JSON serialization of an object"""
+        dict_attributes = self.__dict__
+        dic = {}
+        dic1 = {}
+        for k, v in dict_attributes.items():
+            if isinstance(v, (int, list, dict, bool, str)):
+                if attrs is not None:
+                    if k in attrs:
+                        dic[k] = v
+                else:
+                    dic[k] = v
+        return dic
+
+    def reload_from_json(self, json):
+        dic = self.to_json()
+        for k, v in json.items():
+            if k in dic.keys():
+                dic[k] = v
