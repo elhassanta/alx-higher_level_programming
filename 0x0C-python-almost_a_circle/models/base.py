e6603500200859a687187061a158e7f11182af6a
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-18 13:05:07 -0700

    corr3

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,28 +2,28 @@
 
 """Defines a base model class."""
 import json
-import csv
-import turtle
+
+
 
 
 class Base:
     """Base model.
 
-    This Represents the "base" for all other classes in project 0x0C*.
+    This Represents the base of all classes
 
-    Private Class Attributes:
-        __nb_object (int): Number of instantiated Bases.
+    Private Attributes:
+        __nb_object: Number of instantiated Bases.
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base.
+        """Initializing my class's instance
 
         Args:
-            id (int): The identity of the new Base.
+            id: The identity of the new Base
         """
-        if id is not None:
+        if id != None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -31,10 +31,10 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Return the JSON serialization of a list of dicts.
+        """Return the JSON string
 
         Args:
-            list_dictionaries (list): A list of dictionaries.
+            list_dictionaries: A list of dictionaries
         """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
@@ -42,17 +42,17 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """Write the JSON serialization of a list of objects to a file.
+        """Write the JSON string to a file
 
         Args:
             list_objs (list): A list of inherited Base instances.
         """
         filename = cls.__name__ + ".json"
         with open(filename, "w") as jsonfile:
-            if list_objs is None:
+            if list_objs == None:
                 jsonfile.write("[]")
             else:
-                list_dicts = [o.to_dictionary() for o in list_objs]
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
 
     @staticmethod
