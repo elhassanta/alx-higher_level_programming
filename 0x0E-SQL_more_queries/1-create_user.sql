5af7de17512d038c3b667c4d26b5ba6df295348c
elhassanta <elhassantaibialaoui@gmail.com>
2023-10-18 07:00:32 -0700

    more-database_task1_and_task2-correction

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
+-- script that create a user and grant to him all priliveges to access databse
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
