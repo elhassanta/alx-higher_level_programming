5c43ef206fe171dde9a6b01c9db090fee0125eff
elhassanta <elhassantaibialaoui@gmail.com>
2023-10-18 06:58:06 -0700

    more-database_task1_and_task2

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server
-SELECT user, host, db, select_priv, insert_priv, update_priv, delete_priv, create_priv, drop_priv, grant_priv
-FROM mysql.db
-WHERE user IN ('user_0d_1', 'user_0d_2');
-
+ SHOW GRANTS FOR user_0d_1@localhost;
+ SHOW GRANTS FOR user_0d_2@localhost;
