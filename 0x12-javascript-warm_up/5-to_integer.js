56a5b1e60eb03009bb7eb3c4b7314d8be19e3781
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-13 12:03:23 -0800

    add-tsk5-crr0

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const arg = process.argv;
 if (arg[2]) {
 	const number = parseInt(arg[2]);
 	if (number) {
-		console.log(number);
+		console.log('My number: ',number);
 	}
 	else
 		console.log('Not a number');
