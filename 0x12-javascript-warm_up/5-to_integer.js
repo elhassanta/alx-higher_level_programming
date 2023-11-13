fc7f851f2e88df6f7c280e7c1b50a903842b2eb1
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-13 12:21:23 -0800

    crr-task-5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,11 +2,11 @@
 const arg = process.argv;
 if (arg[2]) {
 	const number = parseInt(arg[2]);
-	if (number) {
-		console.log('My number: ',number);
+	if (isNaN(number) || number === undefined) {
+		console.log('Not a number');
 	}
 	else {
-		console.log('Not a number');
+		console.log('My number: ' ,number);
 	}
 } else {
 	console.log('Not a number');
