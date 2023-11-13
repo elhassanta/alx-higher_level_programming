2e6772c21563f6fccfcf46efe643a9d8e59cbd05
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-13 13:31:42 -0800

    add-tsk-9-c

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	const x = Number(process.argv[2]);
-	let i = 0;
-	let str = '';
-	while (i < x) {
-		str = str + 'X';
-		i++;
-	}
-	i = 0;
-	while (i < x) {
-		console.log(str);
-		i++;
-	}
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
+
