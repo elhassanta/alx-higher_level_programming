8adeea214b5a35ec4a5bfeb6346712eb89f9de6c
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-14 14:14:20 -0800

    add-task6-correction

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,26 +1,14 @@
 #!/usr/bin/node
 
-const Rectangle = require('./5-square');
+const supSquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-  charPrint(c) {
-    if (c) {
-      let x = 0;
-      let str = '';
-      while (x < this.width) {
-        str = str + c;
-        x++;
-      }
-      x = 0;
-      while (x < this.height) {
-        console.log(str);
-        x++;
-      }
-    } else {
-      this.print();
+class Square extends supSquare {
+  charPrint (c) {
+    if (c == null) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
