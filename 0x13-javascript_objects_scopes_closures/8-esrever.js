a19fffa6567b6d1658823f0155308bef3cc18efe
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-14 13:17:13 -0800

    add-task7

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+
 exports.esrever = function (list) {
   let revers = [];
   let len = list.length;
   let x = 0;
   while (x < list.length) {
-    revers[x] = list[len-1];
+    revers[x] = list[len - 1];
     len--;
     x++;
   }
