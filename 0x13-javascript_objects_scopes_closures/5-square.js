7764cf3acd10d41298b8ba704a2291d52f85ff1e
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-14 06:27:11 -0800

    task-5-crr0

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const Rectangel = require('./4-rectangle');
+const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (cote) {
 	  super(cote, cote);
