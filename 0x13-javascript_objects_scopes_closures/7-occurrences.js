d7be7a10646880c1aa136fdf2fecaf94a491c247
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-14 13:04:08 -0800

    add-task7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   let occ = 0;
   list.forEach(function (e) {
     if (e === searchElement) {
