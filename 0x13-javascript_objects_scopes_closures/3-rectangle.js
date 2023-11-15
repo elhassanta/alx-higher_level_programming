af544c55dd0325738901238cbe90080594641480
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-15 09:38:28 -0800

    corr-ts101

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,29 +1,18 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor (w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print() {
-		if (this.width && this.height) {
-			let x = 0;
-			let str = '';
-			while (x < this.width) {
-				str = str + 'X';
-				x++;
-			}
-			x = 0;
-			while (x < this.height) {
-				console.log(str);
-				x++;
-			}
-		}
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Rectangle;
-
