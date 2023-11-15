68abf7aa7dbed83ada71a5f0f78d4ca014c9246b
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-15 11:00:43 -0800

    corr-ts101

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,14 +2,12 @@
 const readFileSync = require('fs').readFileSync;
 const writeFile = require('fs').writeFile;
 const argv = require('process').argv;
-
-const readFromFile = (file) => {
+const getContent = (file) => {
   return readFileSync(file, 'utf8');
 };
 
-const concated = readFromFile(argv[2]) + '' + readFromFile(argv[3]);
+const concated = getContent(argv[2]) + '' + getContent(argv[3]);
 
 writeFile(argv[4], concated, 'utf8', err => {
-  if (error) throw error;
+  if (err) throw err;
 });
-
