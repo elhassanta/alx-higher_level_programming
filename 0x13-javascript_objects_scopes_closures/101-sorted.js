5d30c0a88ee75a62820cca21eb9f2f6b9be8d3fd
elhassanta <elhassantaibialaoui@gmail.com>
2023-11-15 10:35:20 -0800

    corr-ts101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-
+let myDic = require('./101-data.js');
+console.log(myDic);
+let newDic = {};
+Object.entries(myDic).map(e => {
+  if (newDic[e[1]]){
+    newDic[e[1]].push(e[0]);
+  } else {
+    newDic[e[1]] = [e[0]];
+  }
+});
