e8d03ebdf6d89d14a08b23d64c60a15195c63f63
elhassanta <elhassantaibialaoui@gmail.com>
2024-01-03 09:04:02 -0800

    added-first-read-file-with-js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
-
 const fs = require('fs');
-const filePath = process.argv[2];
-fs.readFile(filePath, 'utf8', (err, data) =>
-	{
-		if (err)
-		{
-			console.error(err);
-			return;
-		}
-		console.log('File content:', data);
-	});
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+	if (!error)
+		console.log(error);
+	else
+		console.log(content);
+});
